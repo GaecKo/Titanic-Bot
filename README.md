d977f7232d47c60e09963c215cbd5c18cd1b48fc
Arthur De Neyer <arthur.deneyer@student.uclouvain.be>
2022-06-11 08:48:12 +0200

    updated

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Titanic-Bot
 WIP :)
+
+attack qqun
+défence dans les 5 mins
+
+.help qqun 
+.dishelp qqun 
+
+.top 
---
+++ assets/function.py
@@ -0,0 +1,10 @@
+def help():
+    return """
+
+__Voici l'aide bande de noobs:__
+
+1) **.attack `player` ** -> Attacks the player if 
+
+2) **.defend 
+
+    """
